be006e20b8906f42e1b423aae2f3189fd39e07bd
Joao Vitor <=>
2018-04-17 12:12:17 -0300

    Endpoint /clientes/{id} disponivel

--- src/main/java/com/nelioalves/cursomc/domain/Cliente.java
+++ src/main/java/com/nelioalves/cursomc/domain/Cliente.java
@@ -14,6 +14,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.nelioalves.cursomc.domain.enums.TipoCliente;
 
 
@@ -29,6 +30,7 @@ public class Cliente implements Serializable{
 	private String cpfOuCnpj;
 	private Integer tipo;
 	
+	@JsonManagedReference
 	@OneToMany(mappedBy = "cliente")
 	private List<Endereco> enderecos = new ArrayList<>();
 	
@@ -37,6 +39,7 @@ public class Cliente implements Serializable{
 	private Set<String> telefones = new HashSet<>();
 	
 	
+	@SuppressWarnings("unused")
 	private Cliente() {
 		
 	}
@@ -58,7 +61,7 @@ public class Cliente implements Serializable{
 
 
 	public void setId(Integer id) {
-		id = id;
+		this.id = id;
 	}
 
 
